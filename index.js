aa4393f5c1d0615716dd8a4672a1edb879b1dc43
sameer <sameersahu200138@gmail.com>
2023-11-13 23:27:31 +0530

    completed till game three

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import gradient from 'gradient-string';
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createPromptModule } from "inquirer";
-import {getallCountries} from './utils/country.js'
+import { getallCountries, getallCapitals, getallLandLocked } from './utils/country.js'
+import ora from 'ora';
 
 let game_name;
 
@@ -24,10 +25,10 @@ async function chooseGame() {
     type: 'list',
     message: 'Which game you want to play? \n',
     choices: [
-      'Guess the countries starting with a specific letter',
       'Name n number of countries',
       'Name capitals of differenct countries',
-      'Name curriences of different countries'
+      'Tell whether a country is landlocked or not.',
+      'Guess the countries starting with a specific letter',
     ],
   });
   return handleAnswers(answers.name);
@@ -35,20 +36,20 @@ async function chooseGame() {
 
 function handleAnswers(answers) {
   switch (answers) {
-    case 'Guess the countries starting with a specific letter':
+    case 'Name n number of countries':
       gameOne();
       break;
 
-    case 'Name n number of countries':
-      console.log(2)
-      break;
-
     case 'Name capitals of differenct countries':
-      console.log(3)
+      gameTwo();
       break;
 
-    case 'Name curriences of different countries':
-      console.log(4)
+    case 'Tell whether a country is landlocked or not.':
+      gameThree()
+      break;
+
+    case 'Guess the countries starting with a specific letter':
+      gameFour()
       break;
 
     default:
@@ -57,12 +58,16 @@ function handleAnswers(answers) {
 }
 
 async function gameOne() {
-  let countryCount = 0, strike = 0;
-  let country_lst = getallCountries()
-  async function askCount(){
+  let countryCount = 0, strike = 0, score = 0;
+
+  const spinner = ora('Fetching data').start();
+  let country_lst = await getallCountries();
+  spinner.succeed('Data fetched successfully');
+
+  async function askCount() {
     const answers = await inquirer.prompt({
-      name : 'count',
-      type : 'input',
+      name: 'count',
+      type: 'input',
       message: 'How many countries can you name?',
       default() {
         return 0;
@@ -71,19 +76,194 @@ async function gameOne() {
     countryCount = answers.count
   }
   await askCount()
-  
-  console.log(typeof countryCount, countryCount, country_lst.length)
 
-  if(countryCount <= 0 || countryCount > country_lst.length){
+  if (countryCount <= 0 || countryCount > country_lst.length) {
     console.log(chalk.red(`\n You cant choose a number less than 0 or greater than ${country_lst.len}`));
-    return;
+    return
   }
-  console.log(chalk.bgWhite.bold(`\n lets start you have to name ${countryCount} countries
-  you can choose three wrong chances `))
+  console.log(chalk.green.bold(`\n  lets start you have to name ${countryCount} countries
+  you can choose atmost three wrong answers 
+  start writing :)`))
 
-  
+  while (strike <= 3 || score <= countryCount) {
+
+    if (strike === 3) {
+      console.log(chalk.bgRed(`\n You lost :\(`))
+      return
+    }
+    else if (score == countryCount) {
+      console.log(chalk.bgGreen(`\n You Won :\)`))
+      return
+    }
+
+    let answer;
+    async function askCountry() {
+      const answers = await inquirer.prompt({
+        name: 'userInput',
+        type: 'input',
+        message: 'Write the name of a country: ',
+      });
+      answer = answers.userInput
+    }
+    await askCountry()
+
+    if (country_lst.includes(answer)) {
+      score++
+      country_lst = country_lst.filter(item => item !== answer);
+      console.log(chalk.bgGreen.bold(`\n You are correct, Score: ${score}/${countryCount}`))
+    }
+    else {
+      strike++;
+      console.log(chalk.bgRed(`\n You are incorrect, Stike: ${strike}/3`))
+    }
+
+  }
+
+}
+
+async function gameTwo() {
+  let capitalCount = 0, strike = 0, score = 0;
+
+  const spinner = ora('Fetching data').start();
+  let capitalMap = await getallCapitals();
+  spinner.succeed('Data fetched successfully');
+
+  async function askCount() {
+    const answers = await inquirer.prompt({
+      name: 'count',
+      type: 'input',
+      message: 'How many capitals can you name?',
+    });
+    capitalCount = answers.count
+  }
+  await askCount()
+
+  if (capitalCount <= 0 || capitalCount > capitalMap.size) {
+    console.log(chalk.red(`\n You cant choose a number less than 0 or greater than ${capitalMap.size}`));
+    return
+  }
+  console.log(chalk.bgGreen.bold(`\n  lets start you have to name ${capitalCount} capitals
+  you can choose atmost three wrong answers 
+  start writing :)`))
+
+  while (strike <= 3 || score <= capitalCount) {
+
+    if (strike === 3) {
+      console.log(chalk.bgRed(`\n You lost :\(`))
+      return
+    }
+    else if (score == capitalCount) {
+      console.log(chalk.bgGreen(`\n You Won :\)`))
+      return
+    }
+
+    let answer;
+    async function askCountry(country) {
+      const answers = await inquirer.prompt({
+        name: 'userInput',
+        type: 'input',
+        message: `What is the capital of ${country}: `,
+      });
+      answer = answers.userInput
+    }
+
+    function getRandomKey(map) {
+      const keysArray = Array.from(map.keys());
+      const randomIndex = Math.floor(Math.random() * keysArray.length);
+      return keysArray[randomIndex];
+    }
+    const randomCountry = getRandomKey(capitalMap);
+    await askCountry(randomCountry)
+
+    if (capitalMap.get(randomCountry).includes(answer)) {
+      score++
+      capitalMap.delete(randomCountry)
+      console.log(chalk.bgGreen.bold(`\n You are correct, Score: ${score}/${capitalCount}`))
+    }
+    else {
+      strike++;
+      console.log(chalk.bgRed(`\n You are incorrect, Stike: ${strike}/3`))
+    }
+
+  }
+
+}
+
+async function gameThree() {
+  let landlockedCount = 0, strike = 0, score = 0;
+
+  const spinner = ora('Fetching data').start();
+  let landlockedMap = await getallLandLocked();
+  spinner.succeed('Data fetched successfully');
+
+  async function askCount() {
+    const answers = await inquirer.prompt({
+      name: 'count',
+      type: 'input',
+      message: 'How many landlock country can you identify?',
+    });
+    landlockedCount = answers.count
+  }
+  await askCount()
+
+  if (landlockedCount <= 0 || landlockedCount > landlockedMap.size) {
+    console.log(chalk.red(`\n You cant choose a number less than 0 or greater than ${landlockedMap.size}`));
+    return
+  }
+  console.log(chalk.bgGreen.bold(`\n  lets start you have tell whether a country is landlocked or not.
+  you can write atmost three wrong answers 
+  start writing :)`))
+
+  console.log(chalk.bgGreen.bold(`\n press the letter 'y' if a country is landlocked and 'n' if its not :)`))
+
+
+  while (strike <= 3 || score <= landlockedCount) {
+
+    if (strike === 3) {
+      console.log(chalk.bgRed(`\n You lost :\(`))
+      return
+    }
+    else if (score === landlockedCount) {
+      console.log(chalk.bgGreen(`\n You Won :\)`))
+      return
+    }
+
+    let answer;
+    async function askCountry(country) {
+      const answers = await inquirer.prompt({
+        name: 'userInput',
+        type: 'input',
+        message: `${country} is a landlocked country: `,
+      });
+      answer = answers.userInput
+    }
+
+    function getRandomKey(map) {
+      const keysArray = Array.from(map.keys());
+      const randomIndex = Math.floor(Math.random() * keysArray.length);
+      return keysArray[randomIndex];
+    }
+    const randomCountry = getRandomKey(landlockedMap);
+    await askCountry(randomCountry)
+
+    if (landlockedMap.get(randomCountry) === true && answer === 'y') {
+      score++
+      landlockedMap.delete(randomCountry)
+      console.log(chalk.bgGreen.bold(`\n You are correct, Score: ${score}/${landlockedCount}`))
+    }
+    else if (landlockedMap.get(randomCountry) === false && answer === 'n') {
+      score++
+      landlockedMap.delete(randomCountry)
+      console.log(chalk.bgGreen.bold(`\n You are correct, Score: ${score}/${landlockedCount}`))
+    }
+    else {
+      strike++;
+      console.log(chalk.bgRed(`\n You are incorrect, Stike: ${strike}/3`))
+    }
+  }
+
 }
 
 
-await welcome()
+// await welcome()
 await chooseGame()
--- utils/country.js
+++ utils/country.js
@@ -3,16 +3,61 @@ import axios from 'axios';
 
 export async function getallCountries() {
     try {
-        const response = await axios.get('https://restcountries.com/v3.1/independent?status=true&feilds=name');
-        response = response.json()
-        console.log(response)
-        // temp.forEach((t) => {
-        //     console.log()
-        // })
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+        const country_lst = data.map((e) => e.name.common)
+        return country_lst
+
     } catch (error) {
-        console.error('Error fetching countries:', error.message);
-        return [];
+        console.error('Error fetching country data:', error);
     }
 }
 
-console.log(await getallCountries())
+export async function getallCapitals() {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+        let capitalMap = new Map()
+
+        data.forEach(e => {
+            capitalMap.set(e.name.common, e.capital)
+        });
+        return capitalMap
+    } catch (error) {
+        console.error('Error fetching country data:', error);
+    }
+}
+
+export async function getallLandLocked() {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+        let landlockMap = new Map()
+        data.forEach(e => {
+            landlockMap.set(e.name.common, e.landlocked)
+        });
+        return landlockMap;
+    } catch (error) {
+        console.error('Error fetching country data:', error);
+    }
+}
+
+export async function getByFirstLetter() {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        const data = await response.json();
+        let countryCharacters = new Map()
+        data.forEach(e => {
+            const firstChar = e.name.common[0];
+
+            if (countryCharacters.has(firstChar)) {       
+                countryCharacters.get(firstChar).push(e.name.common);
+            } else {
+                countryCharacters.set(firstChar, [e.name.common]);
+            }
+        });
+        return countryCharacters;
+    } catch (error) {
+        console.error('Error fetching country data:', error);
+    }
+}
